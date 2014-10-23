9432feb0cf906739287b5a6a1a704d48e1434674
徐俊 <18800111918@163.com>
2014-10-23 13:59:40 +0800

    I upload all the DaiLab files to the github!

---
+++ js/publications.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140928
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('publications_files/publicationsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
